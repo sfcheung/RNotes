ae85b2a49dee22c70ab821a8f4a7110c493eed28
Shu Fai Cheung <shufai.cheung@gmail.com>
2014-11-13 00:44:23 +0800

    Add annotations to the sample code. Minor changes.
    Add set.seed() for random data.
    Use scale_size_area() instead of scale_area.
    Make the binned scatterplot example more flexible in setting the scales.

--- Graphs/binned.scatterplot.for.likert.scales.R
+++ Graphs/binned.scatterplot.for.likert.scales.R
@@ -1,10 +1,20 @@
-# Plot a binned scatterplot for discrete ordinal variables (e.g., Likert scale responses)
+# Plot a binned scatterplot for discrete ordinal variables 
+#   (e.g., Likert scale responses)
 
+# Generate the random data for illustration
+set.seed(431432)
 x <- round(runif(100,1,5))
 y <- round(runif(100,1,5))
 xy.data <- data.frame(x,y)
 
 library(ggplot2)
+
+# Assuming the values are integers, set the scale such that each cell is at 
+# the intersection of the values of x and y
+xv <- sort(unique(x))
+yv <- sort(unique(y))
+xv <- c(xv,max(xv)+1) - .5
+yv <- c(yv,max(yv)+1) - .5
+# The graph
 xy.binned <- ggplot(xy.data,aes(x,y))
-v <- 1:6 - .5
-xy.binned + stat_bin2d(breaks=list(x=v,y=v))
+xy.binned + stat_bin2d(breaks=list(x=xv,y=yv))
--- Graphs/bubble.plot.for.likert.scales.R
+++ Graphs/bubble.plot.for.likert.scales.R
@@ -1,13 +1,20 @@
 # Plot a bubble plot for discrete ordinal variables (e.g., Likert scale responses)
 
+# Generate random data for illustration
+set.seed(14897)
 x <- round(runif(100,1,3))
 y <- round(runif(100,-1,1)) + x
 xy.data <- data.frame(x,y)
 
 library(ggplot2)
+
+# Compute the frequencies for all combinations of x and y
 x.values <- sort(unique(xy.data$x))
 y.values <- sort(unique(xy.data$y))
 xbyy <- expand.grid(x.values,y.values)
 xbyy.freq <- as.numeric(xtabs(~x+y, data=xy.data))
+
+# Generate the bubble plot based on the frequencies
 xy.bubble <- ggplot(data.frame(x=xbyy[,1],y=xbyy[,2], freq=xbyy.freq),aes(x,y))
-xy.bubble + geom_point(aes(size=freq)) + scale_area(range=c(0,20))
+xy.bubble + geom_point(aes(size=freq)) + scale_size_area(max_size=30)
+# max_size is used to set the maximum size of the bubbles.
